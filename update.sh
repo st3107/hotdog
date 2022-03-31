f7ffcb50e5002de098460681f06fe92871668c91
st3107 <st3107@columbia.edu>
2022-03-31 11:07:32 -0400

    BLD: add udpate.sh

---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+env="${env:-hotdog}"
+mode="${mode:-user}"
+pyversion="${pyversion:-3.9}"
+
+if [ "$mode" == "user" ]
+then
+    echo "Download latest sotware."
+    git stash
+    git checkout main
+    git pull origin main
+fi
+
+echo "Start installing in the '$env' environment with '$mode' mode."
+conda update -n "$env" -c conda-forge --file requirements.txt --yes
+if [ "$mode" == "developer" ]
+then
+    conda update -n "$env" -c conda-forge --file requirements-dev.txt --freeze-installed --yes
+    conda run -n "$env" --live-stream python -m pip install -e . --no-deps
+else
+    conda run -n "$env" --live-stream python -m pip install . --no-deps
+fi
+
+echo "Finish installation."
+echo "Please activate the conda environment before using the package."
+echo ""
+echo "    conda activate $env"
